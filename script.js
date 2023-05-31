69f53bc2a1fbe37ca19fdc66db749ca9816dd981
Yerlan00 <118426694+Yerlan00@users.noreply.github.com>
2023-05-31 13:06:00 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+
+  loader.classList.add("loader-hidden");
+
+  loader.addEventListener("transitionend", () => {
+    if (document.querySelector(".loader")) {
+      document.body.removeChild(loader);
+    }
+  });
+});
